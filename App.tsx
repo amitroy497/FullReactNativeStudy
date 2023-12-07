f6a362a92b093243b8517f62dd97563318e712cc
Amit Roy <amitroy497@yahoo.com>
2023-12-07 19:38:18 +0530

    2. Button and onPress inn React Native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
+  const pressMe = () => {
+    console.warn('Function called');
+  };
+  const pressMeToo = (val: string) => {
+    console.warn(val);
+  };
   return (
     <View>
       <Text style={[styles.sectionTitle]}>Hello React Native</Text>
       <Text style={[styles.sectionDescription]}>Start ...</Text>
+      <Button title="Press here" color={'green'} onPress={pressMe} />
+      <Button
+        title="Please press me too"
+        color={'red'}
+        onPress={() => console.warn('Another Function Called')}
+      />
+      <Button
+        title="Press"
+        color={'#000'}
+        onPress={() => pressMeToo('Hello Amit')}
+      />
     </View>
   );
 };
